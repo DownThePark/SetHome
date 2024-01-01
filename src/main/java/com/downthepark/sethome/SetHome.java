fb0441ffe2bc0f9cc0db0b9bcbb704c731c53ba1
DownThePark <28867175+DownThePark@users.noreply.github.com>
2023-12-31 19:44:00 -0500

    Add plugin name to update messages Make it clear which plugin is sending the update notifications if the prefix doesn't show.

--- src/main/java/com/downthepark/sethome/SetHome.java
+++ src/main/java/com/downthepark/sethome/SetHome.java
@@ -40,15 +40,16 @@ public class SetHome extends JavaPlugin {
         // Check for updates
         new UpdateChecker(this, 32748).getVersion(version -> {
             if (!getDescription().getVersion().equals(version)) {
-                getLogger().info("There is a new update available! Update available at https://www.spigotmc.org/resources/set-home.32748/");
+                getLogger().info("There is a new update available for SetHome! Update available at https://www.spigotmc.org/resources/set-home.32748/");
             } else {
-                getLogger().info("There are no new updates available.");
+                getLogger().info("There are no new updates available for SetHome");
             }
         });
     }
 
     @Override
     public void onDisable() {
+        // Unregister events
         PlayerRespawnEvent.getHandlerList().unregister(this);
         PlayerMoveEvent.getHandlerList().unregister(this);
     }
