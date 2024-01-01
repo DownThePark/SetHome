5cdd4a41fa0b9c636e3a0801664fcb0e76462a86
DownThePark <28867175+DownThePark@users.noreply.github.com>
2023-12-31 19:43:59 -0500

    Remove old commented-out code

--- src/main/java/com/downthepark/sethome/utilities/MessageUtils.java
+++ src/main/java/com/downthepark/sethome/utilities/MessageUtils.java
@@ -56,36 +56,6 @@ public class MessageUtils {
         }
     }
 
-    /*public void displayMessage(CommandSender sender, MESSAGE_TYPE messageType, String playerName, Integer seconds) {
-        if (messageType == MESSAGE_TYPE.CMD_SETHOME) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_CMD_SETHOME, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.CMD_HOME) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_CMD_HOME, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.CMD_DELHOME) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_CMD_DELHOME, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.MISSING_HOME) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_MISSING_HOME, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.MISSING_WORLD) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_MISSING_WORLD, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.COOLDOWN) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_COOLDOWN, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.WARMUP) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_WARMUP, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.ON_MOVE) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_ON_MOVE, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-        else if (messageType == MESSAGE_TYPE.DENY_CONSOLE) {
-            sender.sendMessage(formatMessage(instance.configUtils.MESSAGE_DENY_CONSOLE, Optional.ofNullable(playerName), Optional.ofNullable(seconds)));
-        }
-    }*/
-
     private String formatMessage(String message, String playerName, Optional<Integer> seconds) {
         String formatted;
 
